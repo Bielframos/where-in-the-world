92924c25aae98b80a2afdd11639696fb89ba503a
Adstrito Design <74715854+Bielframos@users.noreply.github.com>
2022-11-11 14:45:14 -0300

    Update ThemeSwitch component

--- src/components/themeSwitch.svelte
+++ src/components/themeSwitch.svelte
@@ -8,7 +8,9 @@
 </script>
 
 <button
-	class="flex gap-2 px-4 py-2 items-center bg-slate-900/5 hover:bg-slate-900/10 dark:bg-slate-200/5 dark:hover:bg-slate-200/10 rounded-md"
+	class="flex items-center gap-2 rounded-md bg-slate-900/5 px-3 py-2 hover:bg-slate-900/10 dark:bg-slate-200/5 dark:hover:bg-slate-200/10 sm:px-4"
 	on:click={() => handleSwitchDarkMode()}
-	>{#if lightMode}<i class='ph-moon h-5'/> Dark Mode{:else}<i class='ph-sun h-5'/> Dark Mode{/if}</button
+	>{#if lightMode}<i class="ph-moon h-5" />
+		<p class="hidden sm:inline">Dark Mode</p>{:else}<i class="ph-sun h-5" />
+		<p class="hidden sm:inline">Light Mode</p>{/if}</button
 >
